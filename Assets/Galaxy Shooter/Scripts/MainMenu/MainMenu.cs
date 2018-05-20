2b08cf233900cb0a4d32567ec3d37dfa3979eef3
Maria Saveleva <mariiasaveleva@gmail.com>
2018-05-20 10:08:57 +0300

    Added main menu scene

---
+++ Assets/Galaxy Shooter/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlaySinglePlayerGame()
+    {
+        Debug.Log("Loading single player mode scene...");
+    }
+
+    public void PlayCoOpModeGame()
+    {
+        Debug.Log("Loading co-op mode scene...");
+    }
+}
